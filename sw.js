1e3decf40d2c697f3df27334fd2e4a09725caee4
esneider25 <esneiderbencomo@gmail.com>
2026-06-30 20:40:32 -0400

    nada

--- sw.js
+++ sw.js
@@ -1,20 +1,5 @@
-const CACHE_NAME = 'lavadorasbencomo-v2';
-const ASSETS_TO_CACHE = [
-  './',
-  './index.html',
-  './css/style.css',
-  './js/app.js',
-  './js/firebase-config.js',
-  './icon-512.png'
-];
-
 self.addEventListener('install', event => {
-  self.skipWaiting(); // Obliga al SW a activarse inmediatamente
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll(ASSETS_TO_CACHE);
-    }).catch(err => console.log('Error caching assets:', err))
-  );
+  self.skipWaiting();
 });
 
 self.addEventListener('activate', event => {
@@ -22,28 +7,18 @@ self.addEventListener('activate', event => {
     caches.keys().then(cacheNames => {
       return Promise.all(
         cacheNames.map(cache => {
-          // Elimina todas las cachés que no sean la versión actual
-          if (cache !== CACHE_NAME) {
-            console.log('Borrando caché antigua:', cache);
-            return caches.delete(cache);
-          }
+          console.log('Destruyendo caché:', cache);
+          return caches.delete(cache);
         })
       );
-    }).then(() => self.clients.claim()) // Toma el control de todos los clientes de inmediato
+    }).then(() => {
+      self.clients.claim();
+      self.registration.unregister();
+    })
   );
 });
 
 self.addEventListener('fetch', event => {
-  event.respondWith(
-    fetch(event.request)
-      .then(response => {
-        // Guarda en caché la nueva respuesta si es exitosa
-        if (event.request.method === 'GET' && response.status === 200) {
-          const resClone = response.clone();
-          caches.open(CACHE_NAME).then(cache => cache.put(event.request, resClone));
-        }
-        return response;
-      })
-      .catch(() => caches.match(event.request)) // Si no hay internet, usa caché
-  );
+  // Evitar la caché por completo, siempre ir a la red
+  event.respondWith(fetch(event.request));
 });
